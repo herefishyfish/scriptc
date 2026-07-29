7b8accea26f64fb05195671590d869b10ca21a70
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2026-07-29 19:22:54 +0800

    fix(android): update button click listener to use proper type casting

--- tests/fixtures/android-target/counter.ts
+++ tests/fixtures/android-target/counter.ts
@@ -4,10 +4,12 @@ button.setAllCaps(false);
 let count = 0;
 
 button.setText(`Count: ${count}`);
-button.setOnClickListener(() => {
-  count += 2;
-  button.setText(`Count: ${count}`);
-});
+button.setOnClickListener(
+  (() => {
+    count += 2;
+    button.setText(`Count: ${count}`);
+  }) as unknown as android.view.View.OnClickListener,
+);
 const view = new android.widget.LinearLayout(activity);
 view.setOrientation(android.widget.LinearLayout.VERTICAL);
 view.setPadding(0, 160, 0, 0);
